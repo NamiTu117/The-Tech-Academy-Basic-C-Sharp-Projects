b05f320a07c16118381c0e2af78cc3f7d76a57de
NamiTu117 <86804169+NamiTu117@users.noreply.github.com>
2021-11-02 01:13:38 -0400

    Math Input

---
+++ Basic_C#_Programs/MathApplicationAssignment.cs/MathApplicationAssignment.cs/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathApplicationAssignment.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //m
+            Console.WriteLine("Enter a number so we can multiply it!");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The number is " + num * 50);
+            Console.ReadLine();
+
+            //add
+            Console.WriteLine("Enter a number so we can add it!");
+            int add = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The number is " + add + 25);
+            Console.ReadLine();
+
+            //divide
+            Console.WriteLine("Enter a number so we can divide it!");
+            int divide = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The number is " + divide / 12.5);
+            Console.ReadLine();
+
+
+            //remainder 
+            Console.WriteLine("Enter a number so we can get the remainder!");
+            int truce = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The number is " + truce % 7);
+            Console.ReadLine();
+
+            //boolean value equal or less than
+            Console.WriteLine("Enter a number so we can known if it's equal or less than 50!");
+            int mod = Convert.ToInt32(Console.ReadLine());
+            bool number = mod <= 50; 
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/MathandComparison.cs/MathandComparison.cs/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathandComparison.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+            //int difference = 10 - 5;
+            //Console.WriteLine("Five minus Ten = " + difference.ToString());
+            //Console.ReadLine();
+
+            //int remainder = 11 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //bool trueOrflase = 12 < 5;
+            //Console.Write(trueOrflase.ToString());
+            //Console.ReadLine();
+            //Console.ReadLine();s
+
+            //comparisons
+            int roomTempearture = 70;
+            int currentTemperature = 70;
+
+            //bool isWarm = currentTemperature <= roomTempearture;
+            bool isWarm = currentTemperature == roomTempearture;
+            Console.WriteLine(isWarm);
+            Console.ReadLine();
+
+        }
+    }
+}
