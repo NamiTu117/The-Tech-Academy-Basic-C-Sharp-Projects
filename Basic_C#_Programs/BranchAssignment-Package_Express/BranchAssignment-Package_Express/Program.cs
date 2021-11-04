f035309e5c75cc9ea120b0664f0de1e9a2ea8fa4
NamiTu117 <86804169+NamiTu117@users.noreply.github.com>
2021-11-03 21:05:49 -0400

    Branching

---
+++ Basic_C#_Programs/BranchAssignment-Package_Express/BranchAssignment-Package_Express/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchAssignment_Package_Express
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express\n");
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter the weight of your package");
+            int packWeight = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(packWeight + "lbs");
+            Console.ReadLine();
+
+            if (packWeight > 50)
+            {
+                Console.WriteLine("\nPackage to heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine(); 
+
+            }
+            else
+            {
+                Console.WriteLine("\nPlease enter the width of your package");
+                int width = Convert.ToInt32(Console.ReadLine());
+                Console.ReadLine();
+
+                Console.WriteLine("\nPlease enter the height of your package");
+                int height = Convert.ToInt32(Console.ReadLine());
+                Console.ReadLine();
+
+                Console.WriteLine("\nPlease enter the length of your package");
+                int length = Convert.ToInt32(Console.ReadLine());
+                Console.ReadLine();
+
+             
+                if (width > 50 && height > 50 && length > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                    Console.ReadLine(); 
+                }
+                else
+                {
+
+                    int dimensions = width * height * length;
+                    int total = dimensions * packWeight / 100;
+                    Console.WriteLine("Your estimated total for shipping this package is " + "$" + total);
+                    Console.ReadLine();  
+
+
+
+                }
+            }
+                
+           
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/Branches/Branches/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branches
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name!");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your age");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (name == "Tadashi" && age >= 21) 
+            {
+                Console.WriteLine("Hi Tadashi! You're a big boy");
+                Console.ReadLine(); 
+            }
+            else 
+            {
+                Console.WriteLine("Hi! " + name + "Take a seat!");
+                Console.ReadLine();
+            }
+    }
+}
