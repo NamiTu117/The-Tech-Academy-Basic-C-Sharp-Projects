6b6c6e89c13ddc72bc183bf63a2fdbc135e6bf6e
NamiTu117 <86804169+NamiTu117@users.noreply.github.com>
2021-11-04 17:22:10 -0400

    Bool

---
+++ Basic_C#_Programs/BooleanAssignment/BooleanAssignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while loop 
+            int a = 12; 
+            while (a > 1)
+            {
+                Console.WriteLine(a);
+                a--;
+                Console.ReadLine(); 
+            }
+
+            //do while loop
+            int b = 3;
+            do
+            {
+                //b is 3. with b++, it will count upwards 3 times
+                Console.WriteLine(b);
+                b++;
+            }
+            while (b < 4);
+            Console.ReadLine();
+
+
+        }
+    }
+}
