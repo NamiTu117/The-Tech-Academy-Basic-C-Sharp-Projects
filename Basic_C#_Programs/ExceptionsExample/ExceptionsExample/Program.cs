cc0e04b8e4ffb2b41878f28ccffad2c510736c75
NamiTu117 <86804169+NamiTu117@users.noreply.github.com>
2021-11-09 18:23:31 -0500

    NewAssignment

---
+++ Basic_C#_Programs/ExceptionsExample/ExceptionsExample/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exception Handling 
+
+            try
+            {
+                Console.WriteLine("Pick a number");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\nPick a second number");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\nDividing the two..");
+                int numberThree = numberOne / numberTwo;
+                Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+                Console.ReadLine();
+            }
+            catch (FormatException ex) //ex is datatype format exception 
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("\nPlease don't divide by zero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message); 
+            }
+            finally
+            {
+
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ Basic_C#_Programs/StringsAndIntegersAssignment/StringsAndIntegersAssignment/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsAndIntegersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Nums = new List<int>() { 9, 10, 7, 45, 5, 90 };
+            try
+            { 
+
+                //initalizing an integer list               
+                Console.WriteLine("Enter a number");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                   
+                foreach (int num in Nums)
+                {
+                    //takes the number and divides it by other numbers
+                    //on the list
+                    int sum = num / number;
+                    Console.WriteLine(sum); 
+
+                }
+                
+            }            
+            catch (FormatException ex)
+            {
+                //Message warning user against using strings
+                //an int initialized environment
+                Console.WriteLine(ex.Message);
+            }
+            //this exception gives a warning to not divide by zero 
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+            
+        }
+    }
+}
