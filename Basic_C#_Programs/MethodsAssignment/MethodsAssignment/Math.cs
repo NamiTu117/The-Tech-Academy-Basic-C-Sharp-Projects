6ed012e1e6a6258456e743c1e3bfe42ab908a76d
NamiTu117 <86804169+NamiTu117@users.noreply.github.com>
2021-11-10 18:12:50 -0500

    MethodsAssignment

---
+++ Basic_C#_Programs/MethodsAssignment/MethodsAssignment/Math.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment
+{
+    class Math
+    {
+        //methods are integers 
+        public static int Addition(int x)
+        {
+            //addition 
+            int y = x + 10;
+            return y;
+
+            
+            
+        }
+       
+
+        public static int Subtraction(int j)
+        {
+            int i = j - 8;
+            return i; 
+
+        }
+
+        public static int Multiplication(int k)
+        {
+            int m = k * 5;
+            return m; 
+        }
+
+    }
+}
---
+++ Basic_C#_Programs/MethodsAssignment/MethodsAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //asks user to enter a number to add, subtract, and multiply with 
+            Console.WriteLine("What number do you want to do math operations on?");
+            int choice = Convert.ToInt32(Console.ReadLine()); //converts input to integer
+            //entered number (choice) is ran through functions to display results 
+            Console.WriteLine("Your number + 10 is: " + Math.Addition(choice));
+            Console.WriteLine("Your number - 8 is: " + Math.Subtraction(choice));
+            Console.WriteLine("Your number * 5 is: " + Math.Multiplication(choice));
+            Console.ReadLine();
+
+           
+        }
+        
+    }
+}
