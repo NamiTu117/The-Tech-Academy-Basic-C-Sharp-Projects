b05cea5d1f6585d272b5eb5b4a5e57950408dfab
NamiTu117 <86804169+NamiTu117@users.noreply.github.com>
2021-11-11 13:11:54 -0500

    Method Assignment

---
+++ Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/Math2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Math2
+    {
+        public static int Multiplication (int y)
+        {
+            int x = y * 9;
+            return x; 
+        }
+    }
+}
---
+++ Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice = 8;    
+            Math2.Multiplication(choice);
+
+           
+            //Math2 math2 = new Math2();
+
+            //return new Math2().Multiplication(int y); 
+            Console.ReadLine();
+        }
+
+    }
+}
