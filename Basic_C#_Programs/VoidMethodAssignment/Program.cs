f8261450e0ede2a5dd76cc89acfe874e7c975e00
NamiTu117 <86804169+NamiTu117@users.noreply.github.com>
2021-11-12 16:06:03 -0500

    VoidAssigment

---
+++ Basic_C#_Programs/VoidMethodAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiating class
+            Scare scare = new Scare();
+            
+            
+            //calling method passing in two numbers 
+            scare.Jack(9, 1);
+            scare.Jack(x:9, y:1);
+
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/VoidMethodAssignment/Scare.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodAssignment
+{
+    class Scare
+    {
+        //making method 
+        public void Jack(int x, int y)
+        {
+            //performing a math operation using the first parameter
+            int z = x + 10;
+            Console.WriteLine(y);//second parameter is displayed on the screen
+
+            
+        }
+
+        
+    }
+}
