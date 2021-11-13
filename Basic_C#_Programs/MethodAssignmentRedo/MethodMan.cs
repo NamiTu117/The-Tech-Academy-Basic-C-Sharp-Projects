53e086cdd7aab15477f0861862f11a83d397a944
NamiTu117 <86804169+NamiTu117@users.noreply.github.com>
2021-11-12 22:12:57 -0500

    Updates

---
+++ Basic_C#_Programs/MethodAssignmentRedo/MethodMan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignmentRedo
+{
+    class MethodMan
+    {
+        public int Vibe(int x, int y=1)
+        {
+            int z = x + y;
+            return z;
+        }
+    }
+    
+  
+}
---
+++ Basic_C#_Programs/MethodAssignmentRedo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignmentRedo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodMan methodman = new MethodMan(); 
+
+            //prompts user to input two numbers or just one
+            Console.WriteLine("Enter two numbers one at a time, but you don't need to enter anything for the second number");
+            int nums = Convert.ToInt32(Console.ReadLine()); //converts input into integer
+            try
+            {
+                int nums2 = Convert.ToInt32(Console.ReadLine());
+                int result = methodman.Vibe(nums, nums2); //takes number(s) to print out result
+                Console.WriteLine(result);
+            }
+            catch(Exception ex)
+            {
+                int result = methodman.Vibe(nums);
+                Console.WriteLine(result);
+            }
+            
+
+            
+            
+
+            Console.ReadLine();
+        }
+    }
+}
