2f295212ba607bd26317c84bf44abde24ca663d6
NamiTu117 <86804169+NamiTu117@users.noreply.github.com>
2021-11-16 21:22:23 -0500

    OperatorsAssignment

---
+++ Basic_C#_Programs/OperatorsAssignment/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Employee
+    {
+        public string FirstNameID { get; set; }
+        public string LastNameID { get; set; }
+
+        public int Id { get; set; }
+
+        public void Name()
+        {
+            Console.WriteLine(FirstNameID + " " + LastNameID);
+        }
+
+        //overriding == operator 
+        public static bool operator== (Employee employee1, Employee employee2)
+        {
+            return employee1.Id == employee2.Id;
+        }
+
+        public static bool operator!= (Employee employee1, Employee employee2)
+        {
+            return employee1.Id != employee2.Id; 
+        }
+    }
+}
---
+++ Basic_C#_Programs/OperatorsAssignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //instantiating class 
+            Employee employee = new Employee();
+            employee.FirstNameID = "Danny";
+            employee.LastNameID = "Phantom";
+            employee.Id = 1;
+
+            //instantiating object/class
+            Employee employee2 = new Employee();
+            employee2.Id = 1;
+            Console.WriteLine(employee != employee2);
+
+            employee.Name();
+
+            
+        
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+    
+}
