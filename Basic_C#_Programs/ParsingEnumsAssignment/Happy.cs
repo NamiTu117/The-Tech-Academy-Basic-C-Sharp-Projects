faf88d31dc0f81d7bf3fb98961276c36a1853a18
NamiTu117 <86804169+NamiTu117@users.noreply.github.com>
2021-11-18 17:20:32 -0500

    EnumParseAssignment

---
+++ Basic_C#_Programs/ParsingEnumsAssignment/Happy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnumsAssignment
+{
+    class Happy
+    {
+        public enum DayofWeek
+        {
+            //defining enum 
+            Monday=1,
+            Tuesday=2,
+            Wednesday=3,
+            Thursday=4,
+            Friday=5,
+            Saturday=6,
+            Sunday=7 
+        }
+    }
+
+}
---
+++ Basic_C#_Programs/ParsingEnumsAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnumsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //prompting user to enter in current week day
+            Console.WriteLine("Enter the current day of the week");
+            string day = "";
+            //parsing 
+            // DayOfWeek dayofweek = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), day); 
+
+          //reads user input 
+            string value = Console.ReadLine();  
+            try
+            {
+                DayOfWeek dayofweek = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), value);
+            }
+            catch (Exception ex)
+            {
+                //only displays if a day of the week is not entered
+                Console.WriteLine("Please enter an actual day of the week");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+
+        }
+
+    }
+}
