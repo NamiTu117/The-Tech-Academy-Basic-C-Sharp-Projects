4e29ad2744d90743fe11cc72dfde75e072fea6c6
NamiTu117 <86804169+NamiTu117@users.noreply.github.com>
2021-11-23 19:52:39 -0500

    InputFileAssignment

---
+++ Basic_C#_Programs/InputAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; 
+
+namespace InputAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //asking user for input 
+            Console.WriteLine("Type in a number");
+            int numb = Convert.ToInt32(Console.ReadLine());//converts int
+            //converting int to string 
+            string s;
+            s = numb.ToString();
+            //writes number to file         
+            File.WriteAllText(@"E:\GitHubs\The-Tech-Academy-Basic-C-Sharp-Projects\Basic_C#_Programs\number.txt", s);
+            Console.WriteLine(s);
+
+            //printing out results 
+
+
+
+
+
+        }
+    }
+}
