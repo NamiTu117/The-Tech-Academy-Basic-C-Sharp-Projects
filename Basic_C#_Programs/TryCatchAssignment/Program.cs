b83938341394e1aa91fe37002773334425184b12
NamiTu117 <86804169+NamiTu117@users.noreply.github.com>
2021-11-30 14:13:33 -0500

    TryCatchAssignment

---
+++ Basic_C#_Programs/TryCatchAssignment/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //prompting user to enter their age
+                Console.WriteLine("Hi there! Please enter your age");
+                int age = Convert.ToInt32(Console.ReadLine());
+                if (age == 0 || age < 0)
+                {
+                    Console.WriteLine("You cannot enter a zero or a negative number");
+                    return;
+                }
+                DateTime currentYear = DateTime.Now;
+                //turning data into a string to later convert into an integer
+                string year = currentYear.Year.ToString();
+                int ano = Convert.ToInt32(year);
+                int old = ano - age;
+                Console.WriteLine("You were born in {0}", old);
+            
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
+        }   
+}
